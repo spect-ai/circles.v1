8cc1e4ba9a9f3dc83645925d1892251dfdf8fd58
Reshma <reshmashaik3011@gmail.com>
2022-08-02 01:52:27 +0530

    feat: multiple drop down

--- app/modules/Project/ProjectViews/MultipleDropDown.tsx
+++ app/modules/Project/ProjectViews/MultipleDropDown.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconChevronRight, Input, Stack, Text } from "degen";
+import { Box, Button, IconChevronRight, Stack, Text, useTheme, Tag, IconClose } from "degen";
 import { motion, AnimatePresence } from "framer-motion";
 import { matchSorter } from "match-sorter";
 import { FC, useEffect, useRef, useState } from "react";
@@ -12,7 +12,7 @@ export type OptionType = {
 interface Props {
   title?: string;
   options: OptionType[];
-  selected?: OptionType;
+  isOpen: boolean;
   onChange: (option: OptionType) => void;
 }
 
@@ -21,38 +21,81 @@ const OptionsContainer = styled(Box)<{ isExpanded: boolean }>`
   position: absolute;
   z-index: 1;
   width: 20rem;
-  max-height: 15rem;
 `;
 
-const Option = styled(Box)`
+const Option = styled(Box)<{isSelected?: boolean}>`
   cursor: pointer;
+  padding: 0.7rem;
+  margin: 0.1rem;
+  border-radius: 0.5rem;
+  background-color: ${(props) => (props.isSelected ? "rgb(191, 90, 242, 0.2)" : "transparent")};
   &:hover {
-    background-color: rgb(255, 255, 255, 0.1);
+    background-color: rgb(191, 90, 242, 0.2);
   }
 `;
 
-const ScrollContainer = styled(Box)`
+const ScrollContainer = styled(Box)<{mode: string}>`
   ::-webkit-scrollbar {
-    display: none;
+    width: 5px;
+    height: 2rem;
   }
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-  max-height: 15rem;
+  ::-webkit-scrollbar-thumb {
+    background: ${(props) => (props.mode === 'dark' ? "rgb(255, 255, 255, 0.3)" : "rgb(0, 0, 0, 0.2)")};
+  }
+  max-height: 25rem;
   overflow-y: auto;
+  border: 2px solid ${(props) => (props.mode === 'dark' ? "rgb(255, 255, 255, 0.1);" : "rgb(0, 0, 0, 0.1)")};
+  border-radius: 0.5rem;
 `;
+
+const Input = styled.input`
+  background-color: transparent;
+  border: none;
+  padding: 0.8rem;
+  display: flex;
+  border-style: none;
+  border-color: transparent;
+  outline: none;
+  outline-offset: 0;
+  box-shadow: none;
+  font-size: 1rem;
+  caret-color: rgb(191, 90, 242);
+  color: rgb(191, 90, 242);
+  font-weight: 400;
+  width: 8rem;
+  opacity: "40%";
+`
+
+const InputBox = styled(Box)<{mode: string}>`
+  width: 20rem;
+  margin-bottom: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  border: 2px solid ${(props) => (props.mode === 'dark' ? "rgb(255, 255, 255, 0.05);" : "rgb(0, 0, 0, 0.05)")};
+  border-radius: 0.5rem;
+  align-items: center;
+  &:focus-within {
+    border: 2px solid rgb(191, 90, 242);
+  }
+  position: relative;
+`
 
 const slide = {
   hidden: { height: 0, opacity: 0 },
-  open: { height: "10rem", opacity: 1 },
+  open: { height: "0rem", opacity: 1 },
   collapsed: { height: 0, opacity: 0 },
 };
 
-const MultipleDropdown: FC<Props> = ({ options, selected, onChange, title }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+const MultipleDropdown: FC<Props> = ({ options, isOpen, onChange, title }) => {
+  const [isExpanded, setIsExpanded] = useState(isOpen);
   // use input ref
   const inputRef = useRef<HTMLInputElement>(null);
-  const [inputValue, setInputValue] = useState(selected?.id);
+  const [inputValue, setInputValue] = useState([] as string[]);
   const [filteredOptions, setFilteredOptions] = useState(options);
+  console.log(inputValue);
+  
+
+  const {mode} = useTheme();
 
   useEffect(() => {
     setFilteredOptions(options);
@@ -60,49 +103,62 @@ const MultipleDropdown: FC<Props> = ({ options, selected, onChange, title }) => 
 
   return (
     <>
-      <Box style={{ width: "20rem", marginBottom: "0.5rem",}} onClick={(() => setIsExpanded(!isExpanded))} >
-        <Input
-          ref={inputRef}
-          label={title}
-          hideLabel
-          placeholder={title}
-          value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-            setFilteredOptions(
-              matchSorter(options, e.target.value, {
-                keys: ["name"],
-              })
-            );
-          }}
-          suffix={
-            <Button
-              shape="circle"
-              size="small"
-              variant="transparent"
-              onClick={() => {
-                inputRef.current?.focus();
-                setIsExpanded(!isExpanded);
-              }}
-            >
-              <Box
-                transitionDuration="700"
-                style={{
-                  transform: isExpanded ? "rotate(90deg)" : "rotate(0deg)",
-                }}
-              >
-                <IconChevronRight color="textTertiary" size="5" />
+      <InputBox mode={mode}>
+        <Box display="flex" flexDirection="row" flexWrap="wrap" width="64" alignItems="center">
+          {inputValue?.map(value => (
+            <Box onClick={()=> setInputValue(inputValue.filter((i) => i !== value))} padding="0.5">
+              <Tag hover tone="accent" >
+              <Box display="flex" alignItems="center" gap="1">
+                {value}
+                <IconClose size="4" />
               </Box>
-            </Button>
+            </Tag>
+            </Box>
+          ))}
+          <Input
+            ref={inputRef}
+            placeholder={title}
+            onClick={() => setIsExpanded(!isExpanded)}
+            onChange={(e) => {
+              setFilteredOptions(
+                matchSorter(options, e.target.value, {
+                  keys: ["name"],
+                })
+              );
+            }}
+          />
+        </Box>
+        <Box 
+          display="flex" 
+          flexDirection="row" 
+          alignItems="center"
+          style={{
+            position: "absolute",
+            right: "0.5rem",
+          }}
+        >
+          {inputValue.length > 0 && 
+          <Button shape="circle" size="small" variant="transparent" onClick={()=> setInputValue([])}>
+            <IconClose size="4" color="textTertiary"/>
+          </Button>
           }
-        />
-      </Box>
-      <OptionsContainer
-        isExpanded={isExpanded}
-        borderWidth="0.5"
-        backgroundColor="backgroundSecondary"
-        borderRadius="3xLarge"
-      >
+          <Box
+            transitionDuration="700"
+            style={{
+              transform: isExpanded ? "rotate(90deg)" : "rotate(0deg)",
+            }}
+            onClick={() => {
+              inputRef.current?.focus();
+              setIsExpanded(!isExpanded);
+            }}
+          >
+            <Button shape="circle" size="small" variant="transparent">
+              <IconChevronRight color="textTertiary" size="5" />
+            </Button>
+          </Box>
+        </Box>  
+      </InputBox>
+      <OptionsContainer isExpanded={isExpanded} >
         <AnimatePresence>
           {isExpanded && (
             <motion.div
@@ -113,25 +169,40 @@ const MultipleDropdown: FC<Props> = ({ options, selected, onChange, title }) => 
               variants={slide}
               transition={{ duration: 0.3 }}
             >
-              <ScrollContainer>
+              <ScrollContainer 
+                backgroundColor={ mode === 'dark' ? "background" : "white" }
+                mode={mode}
+              >
                 {filteredOptions.map((option) => (
                   <Option
                     key={option.id}
-                    padding="3"
-                    borderRadius="3xLarge"
                     onClick={() => {
                       onChange(option);
-                      setInputValue(option.name);
+                      setInputValue([...inputValue, option.name]);
+                      if (option.name === "") {
+                        setInputValue([]);
+                        return;
+                      }
+                      if (inputValue.includes(option.name)) {
+                        setInputValue(inputValue.filter((i) => i !== option.name));
+                      } else {
+                        if (inputValue.length) {
+                          setInputValue([...inputValue, option.name]);
+                        } else {
+                          setInputValue([option.name]);
+                        }
+                      }
                       setIsExpanded(false);
                     }}
+                    isSelected={inputValue.includes(option.name) ? true : false}
                   >
                     <Stack align="center">
-                      <Text>{option.name}</Text>
+                      <Text color={inputValue.includes(option.name) ? "purple" : "text" }>{option.name}</Text>
                     </Stack>
                   </Option>
                 ))}
                 {!filteredOptions.length && (
-                  <Option padding="4" borderRadius="3xLarge">
+                  <Option>
                     <Stack align="center">
                       <Text variant="label">Not Found</Text>
                     </Stack>
