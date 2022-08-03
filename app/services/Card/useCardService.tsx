4c4021440b7cf471d0ffab77426517a0a656edb8
Aditya Chakraborty <adityachakra16@gmail.com>
2022-08-02 22:36:08 -0500

    refactor: update archive endpoint

--- app/services/Card/useCardService.tsx
+++ app/services/Card/useCardService.tsx
@@ -54,13 +54,16 @@ export default function useCardService() {
 
   const archiveCard = async (cardId: string): Promise<any> => {
     setUpdating(true);
-    const res = await fetch(`${process.env.API_HOST}/card/${cardId}/archive`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
+    const res = await fetch(
+      `${process.env.API_HOST}/card/v1/${cardId}/archive`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }
+    );
     setUpdating(false);
     if (res.ok) {
       void router.push(`/${cId}/${pId}`);
