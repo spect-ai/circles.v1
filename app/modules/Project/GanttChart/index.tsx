076fdd9f72a065e4c13ebb53dd00b9fc73f13bf9
Reshma <reshmashaik3011@gmail.com>
2022-09-06 20:17:47 +0530

    fix: gantt date bug

--- app/modules/Project/GanttChart/index.tsx
+++ app/modules/Project/GanttChart/index.tsx
@@ -13,18 +13,70 @@ function GanttChart() {
   const { updateCard } = useCardService();
   const currentDate = new Date();
 
-  const cards: Task[] = Object.values(project.cards).map((card: CardType) => ({
-    start: card?.startDate
-      ? new Date(card?.startDate)
-      : new Date(currentDate.getFullYear(), currentDate.getMonth(), 1),
-    end: card?.deadline
-      ? new Date(card?.deadline)
-      : new Date(currentDate.getFullYear(), currentDate.getMonth(), 2),
-    name: card.title,
-    id: card.id,
-    type: "task",
-    progress: 0,
-  }));
+  const cards: Task[] = Object.values(project.cards).map(function (
+    card: CardType
+  ) {
+    const dates = {
+      start: currentDate,
+      end: currentDate,
+    };
+    if (!card?.startDate && !card?.deadline) {
+      dates.start = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        1
+      );
+      dates.end = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        2
+      );
+    }
+    if (card?.startDate && !card?.deadline) {
+      dates.start = new Date(card?.startDate);
+      if (new Date(card?.startDate).getDate() > 28) {
+        dates.end = new Date(
+          new Date(card?.startDate).getFullYear(),
+          new Date(card?.startDate).getMonth() + 1,
+          1
+        );
+      } else {
+        dates.end = new Date(
+          new Date(card?.startDate).getFullYear(),
+          new Date(card?.startDate).getMonth(),
+          new Date(card?.startDate).getDate() + 2
+        );
+      }
+    }
+    if (!card?.startDate && card?.deadline) {
+      dates.end = new Date(card?.deadline);
+      if (new Date(card?.deadline).getDate() > 1) {
+        dates.start = new Date(
+          new Date(card?.deadline).getFullYear(),
+          new Date(card?.deadline).getMonth(),
+          new Date(card?.deadline).getDate() - 1
+        );
+      } else {
+        dates.start = new Date(
+          new Date(card?.deadline).getFullYear(),
+          new Date(card?.deadline).getMonth() - 1,
+          29
+        );
+      }
+    }
+    if (card?.startDate && card?.deadline) {
+      dates.start = new Date(card?.startDate);
+      dates.end = new Date(card?.deadline);
+    }
+    return {
+      start: dates.start,
+      end: dates.end,
+      name: card.title,
+      id: card.id,
+      type: "task",
+      progress: 0,
+    };
+  });
 
   const onCardUpdate = async (
     card: CardType,
