195de1dce170930b596bdc6254b4c0c193b6cb51
Aditya Veer <nakulparmar15@gmail.com>
2022-11-05 18:17:10 +0530

    chore: build

--- app/common/components/Editor/styles/theme.ts
+++ app/common/components/Editor/styles/theme.ts
@@ -21,6 +21,7 @@ export const base = {
   fontFamilyMono:
     "'SFMono-Regular',Consolas,'Liberation Mono', Menlo, Courier,monospace",
   fontWeight: 400,
+  zIndex: 1,
   link: colors.primary,
   placeholder: "#B1BECC",
   textSecondary: "#4E5C6E",
