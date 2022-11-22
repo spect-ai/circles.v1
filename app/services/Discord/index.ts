ae5e3d841e8886c70f644d738428bbe7fb3d44cd
Aditya Veer <nakulparmar15@gmail.com>
2022-11-22 15:48:31 +0530

    fix: change bot host env

--- app/services/Discord/index.ts
+++ app/services/Discord/index.ts
@@ -1,13 +1,8 @@
-import { CardType } from "@/app/types";
 import { toast } from "react-toastify";
 
 export const getGuildRoles = async (guildId: string) => {
   const res = await fetch(
-    `${
-      process.env.NODE_ENV === "production"
-        ? "https://spect-discord-bot.herokuapp.com/"
-        : "https://spect-discord-bot.herokuapp.com/"
-    }api/guildRoles?guildId=${guildId}`,
+    `${process.env.BOT_HOST}api/guildRoles?guildId=${guildId}`,
     {
       method: "GET",
     }
@@ -22,8 +17,7 @@ export const getGuildRoles = async (guildId: string) => {
 
 export const fetchGuildChannels = async (guildId: string) => {
   const res = await fetch(
-    "https://spect-discord-bot.herokuapp.com/api/guildChannels?guildId=" +
-      guildId
+    `${process.env.BOT_HOST}/api/guildChannels?guildId=${guildId}`
   );
   if (res.ok) {
     const data = await res.json();
@@ -39,7 +33,7 @@ export const createThread = async (
   guildId: string
 ) => {
   const res = await fetch(
-    `https://spect-discord-bot.herokuapp.com/api/createDiscussionThread?guildId=${guildId}`,
+    `${process.env.BOT_HOST}/api/createDiscussionThread?guildId=${guildId}`,
     {
       headers: {
         "Content-Type": "application/json",
