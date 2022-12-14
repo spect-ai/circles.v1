b277dffedbd0651c9f973a931659b89f6fb11e5b
Aditya Veer <nakulparmar15@gmail.com>
2022-12-14 22:24:46 +0530

    fix: embed fix test

--- pages/r/[formId]/embed.tsx
+++ pages/r/[formId]/embed.tsx
@@ -7,25 +7,38 @@ import {
 import PublicForm from "@/app/modules/PublicForm";
 import useConnectDiscordServer from "@/app/services/Discord/useConnectDiscordServer";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const EmbedPage: NextPage = () => {
   useConnectDiscordServer();
   const context = useProviderCircleContext();
 
-  return (
-    <>
-      <MetaHead
-        title={"Circle"}
-        description={"Circle Description"}
-        image={"Circle Avatar"}
-      />
-      <CircleContext.Provider value={context}>
-        <EmbedFormLayout>
-          <PublicForm />
-        </EmbedFormLayout>
-      </CircleContext.Provider>
-    </>
-  );
+  const router = useRouter();
+
+  if (router.isReady) {
+    return (
+      <>
+        <MetaHead
+          title={"Circle"}
+          description={"Circle Description"}
+          image={"Circle Avatar"}
+        />
+        <CircleContext.Provider value={context}>
+          <EmbedFormLayout>
+            <PublicForm />
+          </EmbedFormLayout>
+        </CircleContext.Provider>
+      </>
+    );
+  }
+
+  return <></>;
 };
+
+export function getServerSideProps() {
+  return {
+    props: {},
+  };
+}
 
 export default EmbedPage;
