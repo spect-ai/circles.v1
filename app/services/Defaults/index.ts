448fb09d5e769733872cf6ae4c93c59a14976be9
TheReshma <reshmashaik3011@gmail.com>
2023-01-20 18:25:29 +0530

    feat: onboard to spect project

---
+++ app/services/Defaults/index.ts
@@ -0,0 +1,14 @@
+export const createDefaultProject = async (id: string) => {
+  const res = await fetch(`${process.env.API_HOST}/collection/v1/${id}/defaultProject`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+  if (res.ok) {
+    const data = await res.json();
+    return data;
+  }
+  return null;
+};
