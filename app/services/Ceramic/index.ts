f4e11b26b5589f35c74c7bb9f3913994ab703e49
Aditya Veer Parmar <nakulparmar15@gmail.com>
2023-02-20 15:47:05 +0530

    chore: update ceramic host

--- app/services/Ceramic/index.ts
+++ app/services/Ceramic/index.ts
@@ -4,7 +4,7 @@ import { EthereumWebAuth, getAccountId } from "@didtools/pkh-ethereum";
 import { DIDSession } from "did-session";
 
 export const compose = new ComposeClient({
-  ceramic: "http://167.99.152.53:7007",
+  ceramic: "https://ceramic-clay.spect.network/node",
   definition: definition as any,
 });
 
