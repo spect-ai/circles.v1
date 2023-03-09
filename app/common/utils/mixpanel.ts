c73e630cfd0c7c4a6445776ad1a472f50b1273e9
Aditya Veer Parmar <nakulparmar15@gmail.com>
2023-03-09 21:55:53 +0530

    fix: mixpanel force track

--- app/common/utils/mixpanel.ts
+++ app/common/utils/mixpanel.ts
@@ -3,6 +3,7 @@ import mixpanel from "mixpanel-browser";
 mixpanel.init(process.env.MIXPANEL_TOKEN || "", {
   debug: true,
   api_host: "https://tracking.spect.network",
+  ignore_dnt: true,
 });
 
 export default mixpanel;
